7c34af0d2cae9dfd2e4330c96c9930c57a3cf5fe
hyde2able <hyde31415@gmail.com>
2016-06-16 22:34:36 +0900

    GoogleMapのスタイル変更

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,7 +174,7 @@
 			scrollwheel: false,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-		var map = new google.maps.Map(document.getElementById('gmap'), options);
+		var map = new google.maps.Map($('#gmap').get(0), options);
 		var style = [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}];
 		var styleType = new google.maps.StyledMapType(style, { name: 'OthloHack' });
 		map.mapTypes.set('OthloHack', styleType);
@@ -191,6 +191,14 @@
 			title: 'OthloHack 会場'
 		};
 		var marker = new google.maps.Marker(markerOpts);
+
+		mapSizeInit();
+	}
+
+	function mapSizeInit() {
+		var width = $('#gmap').width();
+		var margin = ($(window).width() - width) / 2;
+		$('#gmap').css('width', $(window).width()).css('right', margin);	
 	}
 
 	initGmap();
